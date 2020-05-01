f4979f6fcf97791abb40bc16c7f8ec66cce7f8bc
caren1 <czarnocki.w@wp.pl>
2020-05-01 14:25:50 +0200

    rendering an AJAX loading spinner ; while awaiting the results

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,30 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list')
+};
+
+export const elementStrings = {
+    loader: 'loader'
+};
+
+export const renderLoader = parent => {
+    const loader = `
+    <div class="${elementStrings.loader}">
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>`;
+
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+    // delegated to separate object for usability and consistency
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if(loader){
+        // in order to remove the element, we need to move up to parent element
+        loader.parentElement.removeChild(loader);
+    }
 };
